cc8274f992f43a81087849dda69ac7cef6f8f396
Hell0Manu <emanuelle.genesio@estudante.iftm.edu.br>
2024-08-09 13:51:44 -0300

    n menssagens w validacao

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const prevBtns = document.querySelectorAll(".back-button");
 const nextBtns = document.querySelectorAll(".btn-form");
 const progress = document.querySelector(".progress-bar");
 const formSteps = document.querySelectorAll(".slide");
-const progressStaps = document.querySelectorAll(".steps .step")
+const progressStaps = document.querySelectorAll(".steps .step");
+const erro = document.querySelector(".login-error-message");
 
 let formStepNum = 0;
 
@@ -11,15 +12,7 @@ for (let i = 0; i < nextBtns.length; i++) {
     nextBtns[i].addEventListener("click", function (event) {
         event.preventDefault();
         inputsValid = validateInputs(this);
-
-        let name = document.querySelector("#nome").value;
-
-
-          if(!validateForm(name, email, phone)) {
-            
-            return;
-        }
-        else {
+        if (inputsValid) {
             formStepNum++;
             updateFormSteps();
             updateProgressbar();
@@ -33,17 +26,47 @@ function validateInputs(ths) {
     const inputs = ths.parentElement.parentElement.querySelectorAll("input");
     console.log(inputs);
     for (let i = 0; i < inputs.length; i++) {
+        let email = document.querySelector("#email").value,
+            phone = document.querySelector("#phone").value,
+            name = document.querySelector("#nome").value, 
+            company = document.querySelector("#company").value;
+
       
-        const valid = inputs[i].checkValidity();
-        if (!valid) {
+        if (inputs[i].value == "") {
             inputsValid = false;
-            inputs[i].classList.add("login-error-message");
+
+
+            if (name === "") {
+                showError("error-name", "Nome: Preenchimento obrigatório");
+            }
+            if (company === "") {
+                showError("error-company", "Empresa: Preenchimento obrigatório");
+            }
+            if (email === "") {
+                showError("error-email", "Email: Preenchimento obrigatório");
+            }
+            if (phone === "") {
+                showError("error-phone", "Telefone: Preenchimento obrigatório");
+            }
+            
+            inputs[i].style.border = '3px solid #a8200f';
+           
         } else {
-            inputs[i].classList.remove("login-error-message");
+            inputs[i].style.border = '2px solid #c9cbce';
+            erro.style.display = 'none';
         }
+        
     }
     return inputsValid;
 }
+
+function showError(fieldId, message) {
+    const errorDiv = document.getElementById(fieldId);
+    errorDiv.textContent = message;
+    errorDiv.style.display = "block";
+    erro.style.display = 'flex';
+}
+
 
 
 
@@ -58,7 +81,7 @@ prevBtns.forEach(btn => {
 function updateFormSteps() {
     formSteps.forEach(formStep => {
         formStep.classList.contains("active") &&
-        formStep.classList.remove("active");
+            formStep.classList.remove("active");
     });
     formSteps[formStepNum].classList.add("active");
 }
@@ -79,63 +102,58 @@ function updateProgressbar() {
 
 
 
-
 //Recebe os dados do formulario e chama e recebe o resultado da validção 
 function RegisterFormSubmit(event) {
     event.preventDefault();
 
 
-    let email = document.querySelector("#email").value, 
-    phone = document.querySelector("#phone").value, 
-    name = document.querySelector("#nome");
+    let email = document.querySelector("#email").value,
+        phone = document.querySelector("#phone").value,
+        name = document.querySelector("#nome");
 
 
-    
+
 
     //se retornar falso a validação falhou e isso inpedira do cosigo abaixo ser executado 
-    if(!validateForm(name, email, phone))
+    if (!validateForm(name, email, phone))
         return;
 
     //Carregamento
     document.querySelector(".spinner").style.display = "inline-block";
-   
+
     //envia os dados 
-    
+
     sendAsgard(name, email, phone);
-    
-    console.log("Email:", email); 
+
+    console.log("Email:", email);
     console.log("phone:" + phone);
     console.log("nome:" + name);
 }
 
 //Apos validação dos dados faz a criação da conta e manda um negocio para o funil 
-function sendAsgard(name, email, phone) 
-{
+function sendAsgard(name, email, phone) {
     let a = new XMLHttpRequest();
-    (a.withCredentials = !1), 
-    a.open("POST",   "https://3sksqeptx3.esxecute-api.us-east-1.amazonaws.com/prod/site/request-demo"),
-    a.setRequestHeader("Content-Type", "application/json"),(a.onload = function()
-    {
-        if(a.status === 200) 
-        {
-            document.querySelector(".spinner").style.display = "none";
-            document.querySelector(".conta-success-container").style.display = "flex";
-            document.querySelector(".conta-register-container").style.display = "none";
-            document.querySelector("#nameRegister").textContent = name.split(' ')[0] + ", verifique sua caixa de entrada no e-mail";
-            document.querySelector("#emailRegister").textContent = email;
-            unami.track('Signup: Criação de conta');
-        } else 
-        {
-            document.getElementById("errorModal").style.display = "block";
-            unami.track('Erro ' + a.status + ': Criação de conta', 
-            {
-                Event: new Date().toLocaleString('pt-BR', 
-                {
-                    timeZone: 'America/Sao_Paulo'
-                })
-            });
-        }
-    });
+    (a.withCredentials = !1),
+        a.open("POST", "https://3sksqeptx3.esxecute-api.us-east-1.amazonaws.com/prod/site/request-demo"),
+        a.setRequestHeader("Content-Type", "application/json"), (a.onload = function () {
+            if (a.status === 200) {
+                document.querySelector(".spinner").style.display = "none";
+                document.querySelector(".conta-success-container").style.display = "flex";
+                document.querySelector(".conta-register-container").style.display = "none";
+                document.querySelector("#nameRegister").textContent = name.split(' ')[0] + ", verifique sua caixa de entrada no e-mail";
+                document.querySelector("#emailRegister").textContent = email;
+                unami.track('Signup: Criação de conta');
+            } else {
+                document.getElementById("errorModal").style.display = "block";
+                unami.track('Erro ' + a.status + ': Criação de conta',
+                    {
+                        Event: new Date().toLocaleString('pt-BR',
+                            {
+                                timeZone: 'America/Sao_Paulo'
+                            })
+                    });
+            }
+        });
 
     let r = {
         tituloNegocio: company + " - " + name,
@@ -152,43 +170,43 @@ function sendAsgard(name, email, phone)
 }
 
 //Faz a validação dos campos do formulario
-function validateForm(name, email, phone) {
-    let isValid = true;
-    // clearErrors(); 
-    if (name === "") {
-        showError("error-name", "Nome: Preenchimento obrigatório");
-        isValid = false;
-    }
+// function validateForm(name, email, phone) {
+//     let isValid = true;
+//     // clearErrors(); 
+// if (name === "") {
+//     showError("error-name", "Nome: Preenchimento obrigatório");
+//     isValid = false;
+// }
 
-    if(email === "") {
-        showError("error-email", "Email: Preenchimento obrigatório");
-        document.querySelector("#email").style.border = '3px solid #a8200d';
-        document.querySelector(".login-error-message").style.display = "flex";
-        isValid = false; 
-    } else {
-        if(!validateEmail(email)) {
-            showError("error-email", "O e-mail informado não é valido");
-            document.querySelector("#email").style.border = '3px solid #a8200d';
-            document.querySelector(".login-error-message").style.display = "flex";
-            isValid = false;
-        }
-    }
+//     if(email === "") {
+//         showError("error-email", "Email: Preenchimento obrigatório");
+//         document.querySelector("#email").style.border = '3px solid #a8200d';
+//         document.querySelector(".login-error-message").style.display = "flex";
+//         isValid = false; 
+//     } else {
+//         if(!validateEmail(email)) {
+//             showError("error-email", "O e-mail informado não é valido");
+//             document.querySelector("#email").style.border = '3px solid #a8200d';
+//             document.querySelector(".login-error-message").style.display = "flex";
+//             isValid = false;
+//         }
+//     }
 
-    if(phone === "") {
-        showError("error-phone", "Telefone: Preenchimento obrigatório");
-         document.querySelector("#phone").style.border = '3px solid #a8200d';
-            document.querySelector(".login-error-message").style.display = "flex"
-        isValid = false;
-    } else {
-        if(!validatePhone(phone)) {
-            showError("error-phone", "O telefone informado não é valido");
-             document.querySelector("#phone").style.border = '3px solid #a8200d';
-            document.querySelector(".login-error-message").style.display = "flex"
-            isValid = false;
-        }
-    }
-    return isValid;
-}
+//     if(phone === "") {
+//         showError("error-phone", "Telefone: Preenchimento obrigatório");
+//          document.querySelector("#phone").style.border = '3px solid #a8200d';
+//             document.querySelector(".login-error-message").style.display = "flex"
+//         isValid = false;
+//     } else {
+//         if(!validatePhone(phone)) {
+//             showError("error-phone", "O telefone informado não é valido");
+//              document.querySelector("#phone").style.border = '3px solid #a8200d';
+//             document.querySelector(".login-error-message").style.display = "flex"
+//             isValid = false;
+//         }
+//     }
+//     return isValid;
+// }
 
 //Validação do e-mail seguindo o regex fornecido 
 function validateEmail(email) {
@@ -218,25 +236,19 @@ function applyPhoneMask(element) {
 
 
 //Mostra os erros
-function showError(fieldId, message) {
-    const errorDiv = document.getElementById(fieldId);
-    errorDiv.textContent = message;
-    errorDiv.style.display = "block";
-}
 
 //Limpa os erros 
 function clearErrors() {
     document.querySelectorAll(".login-error-message").forEach
-    (
-        function(error) 
-        {
-            error.textContent = "";
-            error.style.display = "none";
-        }
-    )
+        (
+            function (error) {
+                error.textContent = "";
+                error.style.display = "none";
+            }
+        )
 }
 
-document.getElementById("closeModalError").onclick = function() {
+document.getElementById("closeModalError").onclick = function () {
     document.getElementById("errorModal").style.display = "none";
 }
 
